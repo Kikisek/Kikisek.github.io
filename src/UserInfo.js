8b4f3af179e4b0123680315d38c5ef58e965e098
Kristyna Svecova <kristyna@rewardops.com>
2019-02-02 09:32:35 -0500

    Use new mock data, refactor parser to take any number of type params

--- src/UserInfo.js
+++ src/UserInfo.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import {
-  mockData as userdata
-} from './mockData';
+import { mockData as userdata } from './mockData';
 
 export class UserInfo extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      userData: userdata
+      userData: parseUserData(userdata, 'ForkEvent', 'PullRequestEvent')
+      // userData: {}
     }
   }
 
@@ -16,7 +15,7 @@ export class UserInfo extends React.Component {
     // fetch(`https://api.github.com/users/${this.props.username}/events`)
     // .then(response => response.json())
     // .then(json => {
-    //   this.setState({ userData: json });
+    //   this.setState({ userData: parseUserData(json, 'PullRequestEvent', 'ForkEvent') });
     //   console.log(this.state.userData);
     // });
   }
@@ -26,27 +25,37 @@ export class UserInfo extends React.Component {
     return ( 
       <div> 
         <div>Username: {this.props.username} </div>
-        <div>PRs:</div> {parseUserData(this.state.userData, 'PullRequestEvent').map((pr, index) => <div key={index}>{pr.pr.title}</div>)}
+        <div>PRs:</div> {this.state.userData.PullRequestEvent.map((pr, index) => <div key={index}>{pr.pr.title}</div>)}
+        <div>Forks:</div> {this.state.userData.ForkEvent.map((fork, index) => <div key={index}>{fork.repo.name}</div>)}
       </div>
     )
   }
 }
 
-const parseUserData = (data, type) =>
-  data
-  .filter(event => event.type === type)
-  .reduce((acc, current) => {
-    return [
-      ...acc,
-      {
-        repo: {
-        name: current.repo.name,
-        url: current.repo.url
-        },
-        pr: {
-          title: current.payload.pull_request.title,
-          url: current.payload.pull_request.url
-        }
-      }
-    ]
-  }, [])
+const parseUserData = (data, ...type) =>
+  type
+  .reduce((accumulator, currentType) => {
+    return {
+      ...accumulator,
+      [currentType]: [
+        ...data
+        .filter(event => event.type === currentType)
+        .reduce((acc, current) => [
+            ...acc,
+            {
+              repo: {
+              name: current.repo.name,
+              url: current.repo.url
+              },
+              ...(current.type === 'PullRequestEvent' && {
+                pr: {
+                  title: current.payload.pull_request.title,
+                  url: current.payload.pull_request.url
+                }
+              })
+            }
+          ], [])
+      ]
+    }
+  }, {}
+)
