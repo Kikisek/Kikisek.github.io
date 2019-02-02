26400fc936ecd0f9c7d13db4f88633a91ff2a507
Kristyna Svecova <kristyna@rewardops.com>
2019-02-02 00:05:03 -0500

    Create login form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      isLoggedIn: false
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(e) {
+    this.setState({ isLoggedIn: true });
+    e.preventDefault();
+  }
+
+  handleChange(e) {
+    this.setState({ username: e.target.value });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.isLoggedIn ?
+          <div>Logged In</div>
+          :
+          <form onSubmit={this.handleSubmit}>
+          <label>Github Username:
+            <input value={this.state.username} onChange={this.handleChange} required/>
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+        }
       </div>
     );
   }
